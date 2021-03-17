8899ee1f93828c65dbb95183431da9cc7c816bdb
Leo <58913672+ChaosLeo07@users.noreply.github.com>
2021-03-17 12:29:44 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const prefix = '$/';
+
+
+
+const HelpEmbed = new Discord.MessageEmbed()
+    .setTitle("Hilfe")
+    .setDescription("Hier findest du alle Befehle, die der Bot versteht.")
+    .addField("ping", "Zeigt dir die Aktuelle Latenz an.")
+    .addField("warm", "AAAAAAAAAAAAAAAA.")
+    .addField("kalt", "brrrrrrrrrrrrrrrrrrr.")
+    .addField("server", "Zeigt dir Informationen über den Server an.")
+    .addField("help", "Zeigt dir diese Liste an.")
+
+
+    
+client.on('ready', () => {
+    console.log('Bot erfolgreich gestartet!');
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: 'auf 2 Servern | Prefix = "./"',
+            type: 'PLAYING'
+        }
+    })
+    });
+
+
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (message.content.startsWith(prefix)) return;
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.channel.send(`Pong! Die Latenz beträgt im moment ${timeTaken}ms.`);
+      }
+
+      
+    if (command === "help") {
+        message.channel.send(HelpEmbed)  
+     }
+    if (command === "beep") {
+        message.channel.send("boop")
+    }
+
+    if (command === "warm") {
+        message.channel.send("AAAAAAAAA IST DAS HEIß!!")
+    }
+
+    if (command === "kalt") {
+        message.channel.send("brrrrrrr k-k-KALT!")
+    }
+    
+
+    if (command === `server`) {
+        message.channel.send(`Serverinfos:\nServername: ${message.guild.name}\nMitgliederzahl: ${message.guild.memberCount}`);
+    }
+   
+  });
+
+  
+client.login(config.BOT_TOKEN);
